ca65b15aaba39d52b99ab03b9f2887487396413b
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-05-21 22:49:14 -0700

    Add exercise 1.21

--- exercises/first_chapter.scm
+++ exercises/first_chapter.scm
@@ -171,3 +171,20 @@
 (fib 100000)
 (fib 1000000)
 (fib 10000000) ; Got a bit excited
+
+; Exercise 1.21
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
