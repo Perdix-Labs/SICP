4a641333afaa6128525347a032176f99faa6efdf
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-06-08 23:32:25 -0700

    Add solution to 1.29

--- exercises/first_chapter.scm
+++ exercises/first_chapter.scm
@@ -226,3 +226,30 @@
 (search-for-primes 1000000000)
 (search-for-primes 10000000000)
 (search-for-primes 100000000000)
+
+; Exercise 1.29
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b)))
+)
+
+; assumes n is even number
+(define (simpson f a b n)
+  (define (h) (/ (- b a) n))
+  (define (y k) (f (+ a (* k (h)))))
+  (define (term k)
+    (cond ((or (= k 0) (= k n)) (y k))
+          ((even? k) (* 2 (y k)))
+          (else (* 4 (y k)))
+    )
+  )
+  (define (next k) (+ k 1))
+  (* (sum term 0 next n)
+     (/ (h) 3))
+)
+
+(define (cube x) (* x x x))
+
+(simpson cube 0 1 1000)
