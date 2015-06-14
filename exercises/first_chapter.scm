58d61ae5ce796cffa8c1f2fe4bf7fbe74c8666fa
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-06-13 19:04:10 -0700

    Add exercise 1.39

--- exercises/first_chapter.scm
+++ exercises/first_chapter.scm
@@ -438,3 +438,16 @@
   )
 )
 (euler-expansion 100)
+
+; Exercise 1.39
+(define (tan-cf x k)
+  (cont-frac (lambda(i)
+               (if (= i 1)
+                 x
+                 (- (* x x))
+               ))
+             (lambda(i)
+                 (- (* i 2) 1))
+             k)
+)
+(tan-cf 100.0 500)
