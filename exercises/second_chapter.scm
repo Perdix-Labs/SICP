8a0ed1c9c5836b39504dc2a1b63b06434568a85e
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-11-13 19:35:19 -0800

    Add exercise 2.2

--- exercises/second_chapter.scm
+++ exercises/second_chapter.scm
@@ -53,3 +53,43 @@
           (negative? d))
     (make-rat-help (* -1 n) (* -1 d))
     (make-rat-help n d)))
+
+;Exercise 2.2
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (midpoint-segment segment)
+  (define (avg x y)
+    (/ (+ x y)
+       2))
+  (make-point
+    (avg (x-point (start-segment segment)) (x-point (end-segment segment)))
+    (avg (y-point (start-segment segment)) (y-point (end-segment segment)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display " , ")
+  (display (y-point p))
+  (display ")")
+  (newline))
+
+(define (test-midpoint-segment)
+  (print-point (midpoint-segment
+                 (make-segment (make-point 0 0) (make-point 10 0)))))
