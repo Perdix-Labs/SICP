e272472cf49504eed5a34bf39aa09a820d47a2ee
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-11-18 22:39:30 -0800

    Add exercise 2.19

--- exercises/second_chapter.scm
+++ exercises/second_chapter.scm
@@ -255,3 +255,23 @@
 ;Exercise 2.18
 (define (reverse list1)
   (append (reverse (cdr list1)) (list (car list1))))
+
+;Exercise 2.19
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount (except-first-denomination coin-values))
+             (cc (- amount (first-denomination coin-values)) coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (= 0 (length coin-values)))
+
+(define us-coins (list 50 25 10 5 1))
+(cc 100 us-coins) ; answer is 292
