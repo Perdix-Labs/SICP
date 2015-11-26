e488713cdd45dc09d97a5ca81f4845cfd6fcb2d1
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-11-26 13:26:01 -0800

    Add exercise 2.31

--- exercises/second_chapter.scm
+++ exercises/second_chapter.scm
@@ -400,3 +400,13 @@
            (square-tree sub-tree)
            (* sub-tree sub-tree)))
        tree))
+
+;Exercise 2.31
+(define (tree-map fn tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (tree-map fn sub-tree)
+           (fn sub-tree)))
+       tree))
+(define (square x) (* x x))
+(define (square-tree tree) (tree-map square tree))
