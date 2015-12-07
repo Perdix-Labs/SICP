6a342cbb2302fc879daa8e61f48a3c6c6467e408
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2015-12-06 16:17:03 -0800

    Add exercise 2.46

--- exercises/second_chapter.scm
+++ exercises/second_chapter.scm
@@ -624,3 +624,29 @@
         (and (not (attacks? q (car board)))
              (iter q (cdr board)))))
   (iter kth-queen other-queens)))
+
+; Exercise 2.46
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect vect1 vect2)
+  (make-vect (+ (xcor-vect vect1)
+           (xcor-vect vect2))
+        (+ (ycor-vect vect1)
+           (ycor-vect vect2))))
+
+(define (sub-vect vect1 vect2)
+  (make-vect (- (xcor-vect vect1)
+           (xcor-vect vect2))
+        (- (ycor-vect vect1)
+           (ycor-vect vect2))))
+
+(define (scale-vect vect scalar)
+  (make-vect (* scalar (xcor-vect vect))
+             (* scalar (ycor-vect vect))))
