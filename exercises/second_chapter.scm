d600d4f16997010b970954c96edebe9eb4a1b057
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2016-01-27 23:45:02 -0800

    Migrate to use Chicken Scheme, and do exercise 2.49

--- exercises/second_chapter.scm
+++ exercises/second_chapter.scm
@@ -686,3 +686,12 @@
 
 (define (end-segment segment)
   (car (cdr segment)))
+
+; Exercise 2.49
+(use sicp)
+(include "painters.scm")
+
+(write-painter-to-svg (segments->painter outline) "2.49-outline.svg")
+(write-painter-to-svg (segments->painter x) "2.49-x.svg")
+(write-painter-to-svg (segments->painter diamond) "2.49-diamond.svg")
+(write-painter-to-svg (segments->painter wave) "2.49-wave.svg")
---
+++ exercises/segment.scm
@@ -0,0 +1,8 @@
+#!/usr/bin/env chicken-scheme
+(define make-segment cons)
+(define start-segment car)
+(define end-segment cdr)
+
+(define make-point cons)
+(define x-point car)
+(define y-point cdr)
